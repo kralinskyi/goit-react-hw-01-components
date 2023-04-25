b45fbcc5f8faadb1eda1329366ee70734d98fed8
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-04-25 19:07:12 +0300

    profile start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Profile } from './profile/Profile';
+import user from './profile/user.json';
+
 export const App = () => {
   return (
+    //  ! Створення контейнера для компонентів
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        padding: 20,
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        // Відображення інформації про користувача з файлу .json
+        // Передаємо властивості (props) до компонента.
+        userName={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
+
+//! Компонент профілю user'a
+
+export const Profile = ({ userName, tag, location, avatar, stats }) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt={userName} className={css.avatar} />
+        <p className={css.name}>{userName}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li className={css.item}>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
+        </li>
+        <li className={css.item}>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
+        </li>
+        <li className={css.item}>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  userName: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
