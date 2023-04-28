ffd156f54cd1f1adcf368bddeddf3209bc85e383
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-04-28 21:23:19 +0300

    statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
 import { Profile } from './profile/Profile';
 import user from './profile/user.json';
+import { Statistics } from './statistics/Statistics';
+import data from './statistics/data.json';
 
 const styles = {
   display: 'flex',
+  flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
   fontSize: 40,
-  padding: 20,
+  padding: 10,
   color: '#010101',
 };
 
@@ -14,6 +17,7 @@ export const App = () => {
   return (
     <div style={styles}>
       <Profile {...user} />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
