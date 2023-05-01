e9f8f7e4db73831f74304ad9c04e3175d303f66b
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-05-01 20:22:34 +0300

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,7 +26,7 @@ export const App = () => {
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      />
+      />      
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
